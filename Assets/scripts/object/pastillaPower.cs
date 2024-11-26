63b170cfb7e83aa28301cb6b41ff2dd1874df98f
loumtz69 <lucianno.martinez2000@gmail.com>
2024-11-26 15:42:29 -0300

    textos
    je

--- Assets/scripts/object/pastillaPower.cs
+++ Assets/scripts/object/pastillaPower.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class pastilla : MonoBehaviour
 {
+
+    public GameObject candy;
     public bool grajea = false;
     public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("grajea"))
-        {
+        {           
+            candy.SetActive(false);
             grajea = true;
             Destroy(other.gameObject);
             Invoke(nameof(DesactivarGrajea), 5f);
